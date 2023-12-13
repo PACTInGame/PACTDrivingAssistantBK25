e6de77f86be4fc4ff52c29d9c901f29e9c4baaa0
robert <robert.mueller-uri@gmx.de>
2023-12-13 09:39:14 +0100

    fixed most settings and some other bugs

--- KeyboardMouseEmulator.py
+++ KeyboardMouseEmulator.py
@@ -1,24 +1,30 @@
 import pydirectinput
 import keyboard
 
+import pyinsim
+
+SPARE_KEY = "M"
+
 
 def use_keyboard_collision_warning(game_object):
-    pydirectinput.keyDown(game_object.settings.BRAKE_KEY)
+    # Todo check if this is possible somehow
+    game_object.insim.send(pyinsim.ISP_MST,
+                        Msg=b"/key M brake")
+    pydirectinput.keyDown(SPARE_KEY)
     if not game_object.holding_brake:
-        pydirectinput.press(game_object.settings.IGNITION_KEY)
-        print("pressed brake")
+        #pydirectinput.press(game_object.settings.IGNITION_KEY)
         game_object.holding_brake = True
     check_if_user_presses_brake(game_object)
 
 
 def release_brake(game_object):
-    #TODO Fix this (brake is released three times)
     if game_object.holding_brake:
+        game_object.insim.send(pyinsim.ISP_MST,
+                               Msg=b"/key down brake")
         if (not check_if_user_presses_brake(game_object)) or check_if_user_presses_accel(game_object):
-            pydirectinput.keyUp(game_object.settings.BRAKE_KEY)
-            pydirectinput.press(game_object.settings.IGNITION_KEY)
+            pydirectinput.keyUp(SPARE_KEY)
+            #pydirectinput.press(game_object.settings.IGNITION_KEY)
             game_object.holding_brake = False
-            print("released brake")
 
 
 def check_if_user_presses_brake(game_object):
